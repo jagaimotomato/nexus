21aae4a38f98ba344528ea54a948d1c2a1157451
zw <878382660@qq.com>
2026-01-21 15:05:04 +0800

    feat: login

--- web/src/router/index.tsx
+++ web/src/router/index.tsx
@@ -1,6 +1,13 @@
-import React from 'react'
-import { createBrowserRouter, Navigate } from 'react-router'
-import type { RouteObject } from 'react-router'
-import BasicLayout from '@/layouts/BasicLayout'
-import AuthGuard from './authGuard'
-import Login from '@/pags/Login'
+import React from "react";
+import { createBrowserRouter, Navigate } from "react-router";
+import type { RouteObject } from "react-router";
+import BasicLayout from "@/layouts/BasicLayout";
+import AuthGuard from "./authGuard";
+import Login from "@/pages/login/index";
+
+const routes: RouteObject[] = [
+  {
+    path: "/login",
+    element: <Login />,
+  },
+];
